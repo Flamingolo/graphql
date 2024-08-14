19e274868ae6254d9527d6fa47102275dfa52324
Flamingolo <ingo.hark@gmail.com>
2024-08-14 12:06:07 +0300

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  return {
-    base: command === 'serve' ? '/' : '/graphql/', // Use '/' in development and '/graphql/' in production
-    plugins: [react()],
-  }
+export default defineConfig({
+  plugins: [react()],
 })
